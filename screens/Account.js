4f6a1c8ddbcc4da54e92de258ac17d4c39c035e5
Ewan Jordan <ewan.jordan30@gmail.com>
2024-09-08 02:49:45 -0400

    accoutn

--- screens/Account.js
+++ screens/Account.js
@@ -33,58 +33,75 @@ const AccountScreen = () => {
     Alert.alert("Success", "Your account information has been updated.");
     // Here you could add functionality to save the updated values to a server or local storage
   };
-
+  const ProfilePic = ({ letter = "" }) => {
+    return (
+      <View style={styles.profileImg}>
+        <Text style={styles.profileText}>{letter.toUpperCase()}</Text>
+      </View>
+    );
+  };
   const handleEdit = () => {
     setIsEditing(true);
   };
 
   return (
-    <View style={styles.container}>
-      <Text
+    <View style={{ flex: 1 }}>
+      <View
         style={{
-          fontSize: 40,
-          textAlign: "center",
-          paddingVertical: 30,
-          color: "#010101",
-          fontWeight: "900",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 30,
         }}
       >
-        {fullName}
-      </Text>
-      <View style={styles.row}>
-        <Text style={styles.label}>Email:</Text>
-        {isEditing ? (
-          <TextInput
-            style={styles.input}
-            value={email}
-            onChangeText={setEmail}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-        ) : (
-          <Text style={styles.value}>{email}</Text>
-        )}
+        <ProfilePic letter={username[0]}></ProfilePic>
+        <Text
+          style={{
+            fontSize: 40,
+            textAlign: "center",
+            paddingVertical: 30,
+            color: "#010101",
+            fontWeight: "900",
+          }}
+        >
+          {fullName}
+        </Text>
       </View>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Text style={styles.label}>Email:</Text>
+          {isEditing ? (
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          ) : (
+            <Text style={styles.value}>{email}</Text>
+          )}
+        </View>
 
-      <View style={styles.row}>
-        <Text style={styles.label}>Username:</Text>
-        {isEditing ? (
-          <TextInput
-            style={styles.input}
-            value={username}
-            onChangeText={setUsername}
-            autoCapitalize="none"
-          />
-        ) : (
-          <Text style={styles.value}>{username}</Text>
-        )}
+        <View style={styles.row}>
+          <Text style={styles.label}>Username:</Text>
+          {isEditing ? (
+            <TextInput
+              style={styles.input}
+              value={username}
+              onChangeText={setUsername}
+              autoCapitalize="none"
+            />
+          ) : (
+            <Text style={styles.value}>{username}</Text>
+          )}
+        </View>
+
+        <Button
+          title="Log out"
+          style={{ color: "red", fontWeight: "bold" }}
+          onPress={logOut}
+        />
       </View>
-
-      <Button
-        title="Log out"
-        style={{ color: "red", fontWeight: "bold" }}
-        onPress={logOut}
-      />
     </View>
   );
 };
@@ -117,6 +134,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: "#fff",
     flex: 1,
+  },
+  profileImg: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "seagreen",
+  },
+  profileText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white",
   },
 });
 
